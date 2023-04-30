c7b2cfecb2fc6ab7cadd08cce36b838dce68992f
Andrei Kudryavtsev <andreikudrya1995@gmail.com>
2023-04-30 21:58:45 +0500

    Loader Component.

--- app/src/main/java/ru/andreikud/composepractice/MainActivity.kt
+++ app/src/main/java/ru/andreikud/composepractice/MainActivity.kt
@@ -1,76 +1,64 @@
 package ru.andreikud.composepractice
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ru.andreikud.composepractice.ui.theme.ComposePracticeTheme
-import ru.andreikud.composepractice.ui.theme.myColor
-import ru.andreikud.composepractice.ui.theme.myStyle
-import ru.andreikud.composepractice.ui.theme.padding
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposePracticeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
-                }
-            }
+            ScreenContent()
+        }
+    }
+}
+
+@Preview(
+    showSystemUi = true,
+    device = Devices.NEXUS_5X,
+)
+@Composable
+fun LoadingComponentPreview() {
+    ScreenContent()
+}
+
+@Composable
+fun ScreenContent() {
+    ComposePracticeTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(modifier = Modifier.fillMaxSize()) {
+            LoadingComponent(modifier = Modifier.fillMaxSize())
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Column {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier,
-            color = MaterialTheme.colorScheme.myColor,
-            style = MaterialTheme.typography.myStyle,
-        )
-        Spacer(modifier = Modifier.height(MaterialTheme.padding.smallPadding))
-        Text(
-            text = "Hello $name! second time!",
-            modifier = modifier,
-            color = MaterialTheme.colorScheme.myColor,
-            style = MaterialTheme.typography.myStyle,
-        )
-    }
-}
-
-@Preview(
-    showBackground = true,
-    uiMode = UI_MODE_NIGHT_NO,
-)
-@Composable
-fun GreetingPreviewLight() {
-    ComposePracticeTheme {
-        Greeting("Android")
-    }
-}
-
-@Preview(
-    showBackground = true,
-    uiMode = UI_MODE_NIGHT_YES,
-)
-@Composable
-fun GreetingPreviewDark() {
-    ComposePracticeTheme {
-        Greeting("Android")
+fun LoadingComponent(modifier: Modifier = Modifier) {
+    println(MaterialTheme.colorScheme.surface)
+    Surface(
+        color = MaterialTheme.colorScheme.surface, modifier = modifier
+    ) {
+        Box(contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+            Text(
+                text = "This may take a while...", modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .padding(bottom = 16.dp)
+            )
+        }
     }
 }
